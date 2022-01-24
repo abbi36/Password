537c0393e4ef491899cc9f61a4b211f85a36a891
Abbi Engel <abbi.engel33@gmail.com>
2022-01-24 11:05:15 -0600

    First version

---
+++ main.js
@@ -0,0 +1,13 @@
+import input from 'input';
+
+async function askstuff() {
+        const answer = '1Password';
+        const guess = await input.text('What is the password?');
+        if (guess == answer) {
+        console.log('That is the correct password.');
+        } else {
+            console.log('That is the incorrect password.')
+        }
+    }
+askstuff();
+
